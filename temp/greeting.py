ff7fa06f6ac61cb018d50f90b855a3cedbcb5a6a
Han-16 <haanbk16@gmail.com>
2024-11-08 02:07:25 +0900

    ㅁㄴㅇ

--- temp/greeting.py
+++ temp/greeting.py
@@ -6,14 +6,9 @@ def print_greeting():
     store_current_time()
 
 def store_current_time():
-    output_dir = './output'
-    os.makedirs(output_dir, exist_ok=True)
+    output_dir = '/tmp'
+    
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     
-    # 파일 경로 및 시간 저장
     with open(os.path.join(output_dir, 'current_time.txt'), 'w') as file:
         file.write(current_time)
-
-def read_current_time():
-    with open('./output/current_time.txt', 'r') as file:
-        return file.read()
